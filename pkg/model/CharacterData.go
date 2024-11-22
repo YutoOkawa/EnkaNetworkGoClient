70412d48268deea45c6eecf4266274140196ac6b
YutoOkawa <yuutoowata.0305@gmail.com>
2024-11-22 15:36:35 +0900

    Add CharaterData variables

--- pkg/model/CharacterData.go
+++ pkg/model/CharacterData.go
@@ -1,5 +1,21 @@
 package model
 
 type CharacterData struct {
-	Element string `json:"element"`
+	Element         string             `json:"element"`
+	CharacterImage  []string           `json:"Consts"`
+	SkillOrder      []int              `json:"SkillOrder"`
+	Skills          map[string]string  `json:"Skills"`
+	ProudMap        map[string]int     `json:"ProudMap"`
+	NameTextMapHash int                `json:"NameTextMapHash"`
+	SideIconName    string             `json:"SideIconName"`
+	QualityType     string             `json:"QualityType"`
+	WeaponType      string             `json:"WeaponType"`
+	Costumes        map[string]Costume `json:"Costumes,omitempty"`
+}
+
+type Costume struct {
+	SideIconName string `json:"sideIconName"`
+	Icon         string `json:"icon"`
+	Art          string `json:"art"`
+	AvatarId     int    `json:"avatarId"`
 }
