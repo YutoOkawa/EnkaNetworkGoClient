ceef8c01696578a5637869608eb897f654c9b38b
YutoOkawa <yuutoowata.0305@gmail.com>
2024-11-23 19:01:42 +0900

    Implement Mutex to cache

--- pkg/client/cache.go
+++ pkg/client/cache.go
@@ -1,18 +1,30 @@
 package client
 
+import "sync"
+
 type Cache interface {
 	Get(key string) []byte
 	Set(key string, value []byte)
 }
 
 type UserDataCache struct {
+	mutex *sync.RWMutex
 	cache map[string][]byte
 }
 
 func (c *UserDataCache) Get(key string) []byte {
-	return c.cache[key]
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
+	cache, ok := c.cache[key]
+	if !ok {
+		return nil
+	}
+	return cache
 }
 
 func (c *UserDataCache) Set(key string, value []byte) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	c.cache[key] = value
 }
